8edfafef821bcd0c94073ed65dd57e7a02a79d04
manisharma-cs21 <manisharma4844@gmail.com>
2026-03-18 15:51:38 +0530

    Project SetUp Completed

---
+++ devconnect-backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app =express();
+app.use(cors());
+app.use(express.json());
+
+
+const PORT =process.env.PORT ||5000;
+
+app.get("/",(req,res)=>{
+    res.send("Server is running");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}` );
+    
+})
