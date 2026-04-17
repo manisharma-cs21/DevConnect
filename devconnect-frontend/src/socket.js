542cc3202fa307315bb04fa810e0c3cbadb17724
manisharma-cs21 <manisharma4844@gmail.com>
2026-04-17 11:46:01 +0530

    Some changes has been done

--- devconnect-frontend/src/socket.js
+++ devconnect-frontend/src/socket.js
@@ -1,10 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io(" https://devconnect-fqaf.onrender.com", {
+// const socket = io("http://localhost:5000");
+
+const socket = io("https://devconnect-fqaf.onrender.com", {
   transports: ["websocket"],
-}); // backend URL
+});// backend URL
 
 export default socket;
-
-// https://devconnect-fqaf.onrender.com
-//http://localhost:5000/api
